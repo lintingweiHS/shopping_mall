006d9042feba2671f0cae86df11d02ca02cadcbf
ltw <1275501842@qq.com>
2020-07-31 21:03:40 +0800

    fix

--- mall/src/view/oForm.js
+++ mall/src/view/oForm.js
@@ -1,9 +1,41 @@
 import React from 'react'
-export default class oForm extends React.Component{
+export default class oForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name:'',
+            value: ''
+        }
+    }
+    handleChange = (e) => {
+        let key = e.target.name;
+        this.setState({
+            [key] : e.target.value
+        })
+        
+    }
+    handleSubmit = (e) => {
+        console.log(this.state);
+        e.preventDefault();
+    }
 
-    render(){
-        return(
-            <div className="home">123</div>
+
+    render() {
+        return (
+            <div>
+
+                <form action="" onSubmit={this.handleSubmit}>
+                    <label htmlFor="">
+                        名字:
+                        <input type="text" value={this.state.name} onChange={this.handleChange} name="name" />
+                    </label>
+                    <label htmlFor="">
+                        文章
+                        <textarea name="value" id="" value={this.state.value} onChange={this.handleChange} cols="30" rows="10"></textarea>
+                    </label>
+                    <input type="submit" value="提交" />
+                </form>
+            </div>
         )
     }
 }
