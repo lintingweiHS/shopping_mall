9bb48b255e02dbf50ff812c9b10202884119e733
ltw <1275501842@qq.com>
2020-09-21 21:02:08 +0800

    添加商品详情页面

--- web/src/view/shopdetail/index.js
+++ web/src/view/shopdetail/index.js
@@ -3,16 +3,20 @@ import Icon from "@/components/Icon";
 import axios from "_lib/axios.js";
 import qs from "query-string";
 import { Carousel } from "antd-mobile";
-import './index.scss'
+import "./index.scss";
 export default class shopDetail extends Component {
   constructor(props) {
     super(props);
     this.state = {
       id: 0,
       data: ["AiyWuByWklrrUDlFignR", "TekJlZRVCjLFexlOCuWn", "IJOtIlfsYdTyaDTRVrLI"],
-      goods:{
-        images:[]
-      }
+      goods: {
+        images: [],
+      },
+      description: {},
+      cartInfo: {},
+      myScale: 1,
+      num: 1,
     };
   }
   componentDidMount() {
@@ -27,14 +31,49 @@ export default class shopDetail extends Component {
     );
   }
   getData = () => {
-    axios.$Get("api/store/product/rt_meta/" + this.state.id).then((res) => {
-      if(res.success){
-        this.setState({
-          goods:res.data
-        })
-
+    axios.$Get("api/store/product/" + this.state.id).then((res) => {
+      if (res.success) {
+        this.setState(
+          {
+            goods: res.data,
+            num: res.data.minimum || 1,
+          },
+          () => {
+            axios.$Get("api/store/product/rt_meta/" + this.state.id).then((res) => {
+              const goods = { ...this.state.goods, ...res.data };
+              this.setState({
+                goods: goods,
+                num: goods.minimum || 1,
+              });
+            });
+          }
+        );
+        axios.$Get("api/store/product/description/" + this.state.id, null, {
+          hideLoading: true,
+        }).then((res) => {
+          this.setState({
+            description: res.data,
+          });
+          if (this.state.description && this.state.description.scale) {
+            this.calScale(this.state.description.scale);
+          }
+        });
+        axios.$Get("api/store/cart/quantity", {}, { hideLoading: true }).then((res) => {
+          if (res.success) {
+            this.setState({
+              cartInfo: res.data.total,
+            });
+          }
+        });
       }
     });
+  };
+  calScale = (base_width) => {
+    if (base_width > 0) {
+      this.setState({
+        myScale: window.screen.width < base_width ? window.screen.width / base_width : 1,
+      });
+    }
   };
   render() {
     return (
@@ -59,6 +98,10 @@ export default class shopDetail extends Component {
             />
           ))}
         </Carousel>
+        <div className="price">
+          {this.state.goods.price_format}
+          {this.state.goods.orig_url_text}
+        </div>
       </div>
     );
   }
